bf0eb65243218088cd7f7fd1763e2be27a0fb0cc
Cristian Sima <cristian.sima93@gmail.com>
2022-05-17 23:14:42 +0300

    fix the problem

--- src/LoadPaginator.tsx
+++ src/LoadPaginator.tsx
@@ -7,38 +7,7 @@ import selectors from "./selectors";
 import LoadingButton from "./LoadingButton";
 import words from "./words";
 
-type Settings = {
-  key: string;
-  rowsPerLoad: number;
-  manipulateItems: (items: any) => void;
-  resetView: (data: any) => void;
-  changeView: (data: any) => void;
-  requestPage: (page: string, token: string) => any;
-};
-type LoadPaginatorPropTypes = {
-  token: string;
-  children: React.ReactElement<any>;
-  settings: Settings;
-  paginator: any;
-  changeView: (info: {
-    token: string;
-    view: number;
-  }) => void;
-  resetView: (token: string) => void;
-  loadData: (page: number) => void;
-};
-
-type List = {
-  token: string;
-  settings: Settings;
-  paginator: any;
-  changeView: (info: {
-    token: string;
-    view: number;
-  }) => void;
-  resetView: (token: string) => void;
-  loadData: (page: number) => void;
-}
+import { ChangeView, List, LoadData, LoadPaginatorPropTypes, ResetView } from "./LoadPaginatorTypes";
 
 const
   LoadPaginator = (props : LoadPaginatorPropTypes) => {
@@ -76,9 +45,9 @@ const
       }, [list, entities]),
 
       dispatch = useDispatch(),
-      resetView = (data : any) => dispatch(settings.resetView(data)),
-      changeView = (data : any) => dispatch(settings.changeView(data)),
-      loadData = (page : any) => dispatch(settings.requestPage(page, token)),
+      resetView : ResetView = (currentToken) => dispatch(settings.resetView(currentToken)),
+      changeView : ChangeView = (data) => dispatch(settings.changeView(data)),
+      loadData : LoadData = (page) => dispatch(settings.requestPage(String(page), token)),
 
       items = paginator.get("items"),
       hasProblems = paginator.get("hasProblems"),
@@ -106,6 +75,11 @@ const
         }
       };
 
+    // eslint-disable-next-line no-console
+    console.log("paginator", paginator);
+    // eslint-disable-next-line no-console
+    console.log("paginator ? paginator.toJS() : null", paginator ? paginator.toJS() : null);
+
     useEffect(() => {
       if (shouldFetch) {
         loadData(currentPage);
@@ -131,12 +105,12 @@ const
       <>
         {
           React.cloneElement(props.children as React.ReactElement<List>, {
-            token      : props.token,
-            settings   : props.settings,
-            paginator  : props.paginator,
-            changeView : props.changeView,
-            resetView  : props.resetView,
-            loadData   : props.loadData,
+            token    : props.token,
+            settings : props.settings,
+            paginator,
+            changeView,
+            resetView,
+            loadData,
           })
         }
         {
---
+++ src/LoadPaginatorTypes.ts
@@ -0,0 +1,31 @@
+type Settings = {
+    key: string;
+    rowsPerLoad: number;
+    manipulateItems: (items: any) => any;
+    resetView: (data: any) => any;
+    changeView: (data: any) => any;
+    requestPage: (page: string, token: string) => any;
+};
+export type LoadPaginatorPropTypes = {
+    token: string;
+    settings: Settings;
+    children: JSX.Element;
+};
+
+export type ChangeView = (info: {
+    token: string;
+    view: number;
+}) => any;
+
+export type ResetView = (token: string) => any;
+
+export type LoadData = (page: number) => any;
+
+export type List = {
+    token: string;
+    settings: Settings;
+    paginator: any;
+    changeView: ChangeView;
+    resetView: ResetView;
+    loadData: LoadData;
+}
