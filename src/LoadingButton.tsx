a1ffadca3b9e5166e9ebe858a8b77304f11d5655
Cristian Sima <cristian.sima93@gmail.com>
2023-02-06 09:12:07 +0200

    fix

--- src/LoadingButton.tsx
+++ src/LoadingButton.tsx
@@ -3,15 +3,15 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable react/sort-comp */
 import React from "react";
-import { ErrorMessage, LoadingMessage } from "x25/Messages";
 import { useInView } from "react-intersection-observer";
+import { ErrorMessage, LoadingMessage } from "x25/Messages";
+import { words } from "x25/utility";
 
 type LoadingButtonPropTypes = {
   isFetching: boolean;
   hasProblems: boolean;
   fetchMore: () => void;
 };
-import { words } from "x25/utility";
 
 class ErrorBoundary extends React.Component {
   constructor (props : any) {
@@ -31,27 +31,33 @@ class ErrorBoundary extends React.Component {
 }
 
 const
-  LoadingButton = (props : LoadingButtonPropTypes) => {
+  LoadingButton = (props: LoadingButtonPropTypes) => {
     const
       { isFetching, hasProblems, fetchMore } = props,
-      [ref, inView] = useInView({ });
+      [ref, inView] = useInView({}),
+      [fetchMoreCalled, setFetchMoreCalled] = React.useState(false);
 
     React.useEffect(() => {
-      if (!isFetching && !hasProblems && inView) {
+      if (!isFetching && !hasProblems && inView && !fetchMoreCalled) {
         fetchMore();
+        setFetchMoreCalled(true);
       }
-    }, [isFetching, hasProblems, inView]);
+    }, [isFetching, hasProblems, inView, fetchMoreCalled, fetchMore]);
 
     return (
       <div className="text-center my-2">
-        {hasProblems ? <ErrorMessage message={words.ThereWasAProblem} /> : null}
-        {isFetching ? <LoadingMessage message={words.LoadingData} sm /> : (
+        {hasProblems ? (
+          <ErrorMessage message={words.ThereWasAProblem} />
+        ) : null}
+        {isFetching ? (
+          <LoadingMessage message={words.LoadingData} sm />
+        ) : (
           <button
             className="btn btn-link d-print-none"
             disabled={isFetching}
             onClick={fetchMore}
             ref={ref}
-            type="button">
+            type="button" >
             {isFetching ? words.LoadingData : words.LoadMore}
           </button>
         )}
